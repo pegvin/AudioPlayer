e9d9753e893f607c62e3efe3c5a0b4d27d51771c
pegvin <75035219+pegvin@users.noreply.github.com>
2023-10-30 00:17:54 +0530

    Make Fixed Size & Pos Window

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,17 @@ int main(void) {
 	ImBase::Window::Init(320, 240, "AudioPlayer", false);
 	ImBase::Window::SetMaxFPS(50);
 
+	ImGuiIO& io = ImGui::GetIO();
+
 	while (!ImBase::Window::ShouldClose()) {
 		ImBase::Window::NewFrame();
 
-		ImGui::Text("Hello World!");
+		ImGui::SetNextWindowPos({ 0, 0 });
+		ImGui::SetNextWindowSize(io.DisplaySize);
+		if (ImGui::Begin("Main", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse)) {
+			ImGui::Text("Hello World!");
+			ImGui::End();
+		}
 
 		ImBase::Window::EndFrame();
 	}
